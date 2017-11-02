b9d3a11e045331304aa636937e0cdfb045720160
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-02 23:58:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
+    puts params
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     #{}"#{@user.name} #{@user.email} #{@user.password}"
     @user.save
@@ -28,7 +29,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-
+    puts params
+    @user = User.find_by(email: params["email"], password: params["password"])
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
